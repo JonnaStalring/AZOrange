649993450b3c9413e295b977433f6e65561b6a4b
JonnaStalring <jonna.stalring@gmail.com>
2015-06-23 09:21:27 +0200

    Don't hard code rdkit dir name.

--- install/install.py
+++ install/install.py
@@ -754,7 +754,7 @@ application/xml=AZOrange.desktop;
             self.successInstall = False
             return 
         # Names no longer follow the same convention. Hard coded name of tar extract.
-        unpackDir = "rdkit-Release_2015_03_1"
+        #unpackDir = "rdkit-Release_2015_03_1"
         self.__logAndExecute(UnpackCmd + tarFile)
         self.__logAndExecute("mv " + unpackDir + " " + name )
 
